775400e3f64ca2dadeb3a26f840f83096b388f75
markusms <33954500+markusms@users.noreply.github.com>
2018-04-05 13:26:48 +0300

    Started motor control

--- ZumoBot.cydsn/main.c
+++ ZumoBot.cydsn/main.c
@@ -56,18 +56,23 @@ int rread(void);
 //battery level//
 int main()
 {
+    struct sensors_ ref;
+    struct sensors_ dig;
+    
     CyGlobalIntEnable; 
     UART_1_Start();
     Systick_Start();
     
     ADC_Battery_Start();        
 
-    int time = 0, timesCheckedBattery = 1, ledOn = 0;
+    int time = 0, timesCheckedBattery = 1, ledOn = 0; //akkumuuttujia
     int16 adcresult =0;
     float volts = 0.0;
+  
+    reflectance_start();
+    reflectance_set_threshold(9000, 9000, 11000, 11000, 9000, 9000); // set center sensor threshold to 11000 and others to 9000
     
-
-    printf("\nBoot\n");
+    printf("\nBEEP BOOP\n");
 
     //BatteryLed_Write(1); // Switch led on 
     BatteryLed_Write(0); // Switch led off 
@@ -75,11 +80,14 @@ int main()
     //button = SW1_Read(); // read SW1 on pSoC board
     // SW1_Read() returns zero when button is pressed
     // SW1_Read() returns one when button is not pressed
-
+    
+    motor_start(); //start the motor
+    
     for(;;)
     {
+        //Battery + LED
         time = GetTicks()/1000; //seconds
-        if (time > (5*timesCheckedBattery))
+        if (time > (10*timesCheckedBattery)) //go here every 10 seconds
         {
             ADC_Battery_StartConvert();
             if(ADC_Battery_IsEndConversion(ADC_Battery_WAIT_FOR_RESULT)) {   // wait for get ADC converted value
@@ -91,29 +99,67 @@ int main()
                 volts = (float) adcresult/4095*5*1.5;
                 printf("%d %f V\r\n",adcresult, volts);
             }
-           
-            timesCheckedBattery++;
+            timesCheckedBattery++; //variable that tells how many times the battery has been checked. Used for checking the battery every 5 seconds.
         }
-        if (volts < 4)
+        if (volts < 4) //if battery is too low keep flashing the led
         {
-            if (ledOn == 0)
+            if (ledOn == 0) //if led is off, turn the led on
             {
                 BatteryLed_Write(1);
                 ledOn = 1;
             }
-            else
+            else //if led is on turn the led off
             {
                 BatteryLed_Write(0);
                 ledOn = 0;
             }
             CyDelay(200);
         }
-        if (volts >= 4 && ledOn == 1)
+        if (volts >= 4 && ledOn == 1) //if battery is back to over 4 and led was left on, turn it off
         {
             BatteryLed_Write(0);
             ledOn = 0;
         }
+        
+        //Line reading with motor control
+        // read raw sensor values
+        reflectance_read(&ref);
+        //printf("%5d %5d %5d %5d %5d %5d\r\n", ref.l3, ref.l2, ref.l1, ref.r1, ref.r2, ref.r3);       // print out each period of reflectance sensors
+        //r3 = oikea reuna, 13 = vasen reuna
+        // read digital values that are based on threshold. 0 = white, 1 = black
+        // when blackness value is over threshold the sensors reads 1, otherwise 0
+        reflectance_digital(&dig); //print out 0 or 1 according to results of reflectance period
+        //printf("%5d %5d %5d %5d %5d %5d \r\n", dig.l3, dig.l2, dig.l1, dig.r1, dig.r2, dig.r3);        //print out 0 or 1 according to results of reflectance period
+        //printf("L1 sensori: %5d L2: %5d L3: %5d \n", ref.l1, ref.l2, ref.l3);
+        //printf("R1 sensori: %5d R2: %5d R3: %5d \n", ref.r1, ref.r2, ref.r3);
+        printf("L2 sensori: %5d\n",ref.l2);
+        printf("R2 sensori: %5d\n",ref.r2);
+        /*if (ref.l1 > 10000 && ref.r1 > 10000) //both middle see, we are going forwards
+        {
+            motor_forward(30,50);           
+        }*/
+        /*else if (ref.l1 > 10000 && ref.l2 > 10000) //left mid side sees black -> (small) turn right
+        {
+            motor_turn(50,1,100);
+        }
+        else if (ref.r1 > 10000 && ref.r2 > 10000) //right mid side sees black -> (small) turn left
+        {
+            motor_turn(1,50,100);
+        }*/
+         if (ref.l2 > 15000) //left mid side sees black -> (small) turn right
+        {
+            motor_turn(30,15,10);
+        }
+        else if (ref.r2 > 15000) //right mid side sees black -> (small) turn left
+        {
+            motor_turn(15,30,10);
+        }
+        else
+        {
+            motor_forward(20,10);
+        }
     }
+    motor_stop();  //stop the motor
  }   
 #endif
 
@@ -226,7 +272,7 @@ int main()
         // read raw sensor values
         reflectance_read(&ref);
         printf("%5d %5d %5d %5d %5d %5d\r\n", ref.l3, ref.l2, ref.l1, ref.r1, ref.r2, ref.r3);       // print out each period of reflectance sensors
-        
+        //r3 = oikea reuna, 13 = vasen reuna
         // read digital values that are based on threshold. 0 = white, 1 = black
         // when blackness value is over threshold the sensors reads 1, otherwise 0
         reflectance_digital(&dig);      //print out 0 or 1 according to results of reflectance period
@@ -247,16 +293,56 @@ int main()
 
     motor_start();              // motor start
 
-    motor_forward(100,2000);     // moving forward
-    motor_turn(200,50,2000);     // turn
-    motor_turn(50,200,2000);     // turn
-    motor_backward(100,2000);    // movinb backward
+    //motor_forward(255,4000);     // moving forward
+    //motor_turn(255,50,1000);     // turn
+    //motor_forward(255,4000);
+    //motor_turn(50,255,1000);     // turn
+    //motor_backward(100,2000);    // movinb backward
+    
+        motor_forward(255,2000);
+        motor_turn(255,1,700);
+        motor_forward(255,2000);
+        motor_turn(1,255,700);
+    
+        
+        motor_forward(122,1000);
+        motor_turn(122,1,1400);
+        motor_forward(122,1000);
+        motor_turn(1,122,1400);
+        
+        
+        motor_forward(255,1000);
+        motor_backward(255,500);
+        motor_backward(150,500);
+        motor_backward(75,500);
+        
+        
+        
+        motor_forward(255,2000);
+        motor_turn(255,1,700);
+        motor_forward(255,2000);
+        motor_turn(1,255,700);
+    
+        
+        motor_forward(122,1000);
+        motor_turn(122,1,1400);
+        motor_forward(122,1000);
+        motor_turn(1,122,1400);
+        
+        
+        motor_forward(255,1000);
+        motor_backward(255,500);
+        motor_backward(150,500);
+        motor_backward(75,500);
+    
        
     motor_stop();               // motor stop
     
     for(;;)
     {
 
+        
+        
     }
 }
 #endif
